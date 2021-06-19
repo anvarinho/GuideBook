d27f9c39d4a5420f68321e87a9ccbf903ae37dd7
anvarinho <anvarinho@gmail.com>
2021-06-19 11:40:33 +0600

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Anvar Jumabaev on 19.06.21.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
